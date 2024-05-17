05e4bee34fdadd46da81af52ac2d7b7e358761af
TinyBFG <76491433+TinyBFG@users.noreply.github.com>
2024-05-17 13:17:57 -0500

    Needed to accept floats instead of integers on weight and price fields.

--- lib/cat_food/products.ex
+++ lib/cat_food/products.ex
@@ -4,13 +4,14 @@ defmodule Cat_Food.Products do
   schema "product" do
     field :brand, :string
     field :product_name, :string
-    field :weight, :integer
-    field :price, :integer
+    field :weight, :float
+    field :price, :float
   end
 
   def changeset(product, params \\ %{}) do
     product
     |> Ecto.Changeset.cast(params, [:brand, :product_name, :weight, :price])
-    |> Ecto.Changeset.validate_required([:brand])
+    |> Ecto.Changeset.validate_required([:brand, :product_name])
   end
+
 end
